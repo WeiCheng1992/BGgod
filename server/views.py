7e1aa2fa50336d23d1e9b168a6c607c4dfddb6c9
cw272 <cw272@duke.edu>
2016-02-23 01:30:15 -0500

    login

--- server/views.py
+++ server/views.py
@@ -1,23 +1,16 @@
-from flask import render_template
+from flask import render_template, session, request
 from server import app
-from server.forms import LoginForm
 
 
 @app.route('/', methods = ['GET'])
 @app.route('/index', methods = ['GET'])
 def index():
-    user = {'nickname': 'Charles'}
     return render_template("index.html",
         title = 'Home',
         user = user)
 
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html',
-        title = 'Sign In',
-        form = form)
+
 
 
 def launch():
